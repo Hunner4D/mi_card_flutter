609063b1382e66244eb661241d1e583b7d837ed7
Hunter Herrera <h.herrera.academics@gmail.com>
2021-06-30 13:45:23 -0700

    Containers as children of column

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,40 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+            child: Column(
+          children: [
+            Container(
+              color: Colors.white,
+              child: Text('Hello'),
+              height: 100,
+              width: 100,
+              margin: EdgeInsets.symmetric(vertical: 50, horizontal: 100),
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.blue,
+              child: Text('Second Container'),
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.red,
+              child: Text('Second Container'),
+            ),
+          ],
+        )),
       ),
-    ),
-  );
+    );
+  }
 }
